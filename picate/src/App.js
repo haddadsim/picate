77d84ee7c21505250501bbfce6cc7ec081d904ff
haddadsim <haddadsimon@outlook.com>
2019-07-24 14:09:54 +0300

    feat: add index.js file

---
+++ picate/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Next Gen Photo Generator
+        </p>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
