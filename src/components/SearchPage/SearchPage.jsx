9467e9d3afc72ebdcabc8a00f067c9d9dddf6dc6
haddadsim <haddadsimon@outlook.com>
2019-07-30 14:27:39 +0300

    feat: add API to search for images

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import SearchBar from '../SearchBar/SearchBar';
+
+const API_KEY = process.env.REACT_APP_ACCESS_KEY;
 
 const SearchPage = () => {
   const [searchText, setSearchTerm] = useState('');
@@ -8,9 +11,20 @@ const SearchPage = () => {
     setSearchTerm(e.target.value);
   };
 
+  const fetchImages = () => {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: searchText },
+      headers: {
+        Authorization: API_KEY,
+      },
+    });
+  };
+
   const onSubmitHandler = (e) => {
     e.preventDefault();
+    fetchImages();
   };
+
   return (
     <SearchBar
       onSubmitHandler={onSubmitHandler}
