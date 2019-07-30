e779f610dde519f718ff68cc19a5daeef3770831
haddadsim <haddadsimon@outlook.com>
2019-07-30 10:29:46 +0300

    fix: remove hooks from SearchBar to implement in App.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './SearchBar.css';
 
-const SearchBar = () => {
-  const [search, setSearch] = useState('');
-  return (
-    <form>
-      <input className="Search" placeholder="Search Images" />
-      <button type="submit" id="submit" className="search-button">
-        <i className="icon">search</i>
-      </button>
-    </form>
-  );
-};
+const SearchBar = () => (
+  <form>
+    <input type="search" className="Search" placeholder="Search Images" />
+    <button type="submit" className="search-button">
+      <i className="icon">search</i>
+    </button>
+  </form>
+);
 export default SearchBar;
