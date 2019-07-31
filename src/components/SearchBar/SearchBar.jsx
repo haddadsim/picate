7a8a874babd1c557a0dd08350806abd67cd95521
haddadsim <haddadsimon@outlook.com>
2019-07-31 13:48:20 +0300

    feat: add Material UI to search bar and button

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,19 +1,26 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import './SearchBar.css';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
 
 const SearchBar = ({ onSubmitHandler, searchText, onInputChange }) => (
   <form onSubmit={onSubmitHandler}>
-    <input
+    <Input
       type="search"
       className="Search"
       placeholder="Search Images"
       value={searchText}
       onChange={onInputChange}
     />
-    <button type="submit" className="search-button">
+    <Button
+      type="submit"
+      className="search-button"
+      color="primary"
+      variant="contained"
+    >
       <i className="icon">search</i>
-    </button>
+    </Button>
   </form>
 );
 export default SearchBar;
