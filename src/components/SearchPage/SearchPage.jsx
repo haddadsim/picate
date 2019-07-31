d79acb3b3e8a79c4164b4418351d579bcf97626b
haddadsim <haddadsimon@outlook.com>
2019-07-31 18:14:13 +0300

    fix: add no images found if empty api call

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import SearchBar from '../SearchBar/SearchBar';
 import ImageList from '../ImageList/ImageList';
+import './SearchPage.css';
 
 const API_KEY = process.env.REACT_APP_ACCESS_KEY;
 
 const SearchPage = () => {
   const [searchText, setSearchTerm] = useState('');
-  const [image, setImage] = useState([]);
+  const [image, setImage] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const onInputChange = (e) => {
     setSearchTerm(e.target.value);
@@ -22,22 +24,27 @@ const SearchPage = () => {
     })
       .then((data) => {
         setImage(data.data.results);
+        setIsLoaded(false);
       });
   };
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
     fetchImages();
+    setIsLoaded(true);
   };
 
   return (
     <div>
       <SearchBar
+        className="search-bar"
         onSubmitHandler={onSubmitHandler}
         onInputChange={onInputChange}
         searchText={searchText}
       />
-      <ImageList image={image} />
+      <div className="image-container">
+        {image && <ImageList image={image} isLoaded={isLoaded} />}
+      </div>
     </div>
 
   );
