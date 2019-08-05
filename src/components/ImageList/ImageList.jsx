73e67e304a5b0b3a1ac364a278ecb81b62ee245c
haddadsim <haddadsimon@outlook.com>
2019-08-05 16:52:39 +0300

    feat: add lightbox-gallery

--- src/components/ImageList/ImageList.jsx
+++ src/components/ImageList/ImageList.jsx
@@ -1,10 +1,17 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 import ReactLoading from 'react-loading';
 import './ImageList.css';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 
 const ImageList = ({ image, isLoaded }) => {
+  const [imageIndex, setImageIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState('false');
+
   if (isLoaded) {
     return (
       <div className="spinner">
@@ -13,12 +20,19 @@ const ImageList = ({ image, isLoaded }) => {
     );
   }
 
-  const imgs = image.map(img => (
+  const onClickHandler = (e) => {
+    setIsOpen(true);
+    setImageIndex(e.target.id);
+    e.preventDefault();
+  };
+  console.log(imageIndex);
+  const imgs = image.map((img, index) => (
     <img
+      id={index}
       key={img.id}
       src={img.urls.small}
+      onClick={onClickHandler}
     />
-
   ));
 
   if (imgs.length === 0) {
@@ -26,10 +40,26 @@ const ImageList = ({ image, isLoaded }) => {
       <p>No images</p>
     );
   }
+
+  if (isOpen === true) {
+    return (
+      <Lightbox
+        onCloseRequest={() => setIsOpen(false)}
+        mainSrc={image[imageIndex].urls.regular}
+        onMoveNextRequest={() => setImageIndex((imageIndex + 1) % image.length)}
+        onMovePrevRequest={() => setImageIndex((imageIndex + image.length - 1) % image.length)}
+        nextSrc={image[(imageIndex + 1) % image.length].urls.regular}
+        prevSrc={image[(imageIndex + image.length - 1) % image.length]}
+
+      />
+    );
+  }
+
   return (
     <React.Fragment>
       {imgs}
     </React.Fragment>
+
   );
 };
 
