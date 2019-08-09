bce7a8094de3bd4208f49b4ae7b2f438594cebd9
haddadsim <haddadsimon@outlook.com>
2019-08-09 14:56:26 +0300

    feat: add pagination to call for more photos

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,27 +1,22 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import SearchBar from '../SearchBar/SearchBar';
 import ImageList from '../ImageList/ImageList';
 import './SearchPage.css';
-
-const API_KEY = process.env.REACT_APP_ACCESS_KEY;
+import fetchImages from '../../utils/index';
 
 const SearchPage = () => {
   const [searchText, setSearchTerm] = useState('');
   const [image, setImage] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [isNext, setIsNext] = useState(false);
+  const [nextPageIndex, setNextPageIndex] = useState(1);
 
   const onInputChange = (e) => {
     setSearchTerm(e.target.value);
   };
 
-  const fetchImages = () => {
-    axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: searchText },
-      headers: {
-        Authorization: API_KEY,
-      },
-    })
+  const getImages = () => {
+    fetchImages(nextPageIndex, searchText)
       .then((data) => {
         setImage(data.data.results);
         setIsLoaded(false);
@@ -30,10 +25,25 @@ const SearchPage = () => {
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
-    fetchImages();
+    setNextPageIndex(1);
+    getImages();
     setIsLoaded(true);
   };
 
+  const onClickHandler = () => {
+    setIsNext(true);
+    setNextPageIndex(parseInt(nextPageIndex + 1, 10));
+  };
+
+  if (isNext === true) {
+    fetchImages(nextPageIndex, searchText)
+      .then((data) => {
+        const result = data.data.results;
+        setImage(image.concat(result));
+        setIsLoaded(false);
+      });
+    setIsNext(false);
+  }
   return (
     <React.Fragment>
       <SearchBar
@@ -46,6 +56,11 @@ const SearchPage = () => {
       <div className="image-container">
         {image && <ImageList image={image} isLoaded={isLoaded} />}
       </div>
+      <button
+        type="submit"
+        onClick={onClickHandler}
+        value="next"
+      />
     </React.Fragment>
 
   );
